c308385c03af88e5366be41f39c9c4fc64b70efb
Sonal kumari <115936469+Sonalkri2004@users.noreply.github.com>
2024-10-11 12:57:16 +0530

    homepage modern ui added.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,8 @@ import HomePage from "./pages/HomePage";
 export default function App() {
   return (
     <>
-      <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden"> */}
+      <div>
         {/* BG */}
         {/* <div className="fixed inset-0 z-0">
           <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
