be4d29161747d6f4f144c7f17aa7e8247ca36ff1
Durgesh Prasad <durgeshhh.io@gmail.com>
2024-10-31 10:34:50 +0530

    err fixed

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,41 @@
+// import express from 'express'
+// import dotenv from 'dotenv'
+// import cors from 'cors'
+// import cookieparser from 'cookie-parser'
+// import DbCon from './utlis/db.js'
+// import AuthRoutes from './routes/Auth.js'
+// import AdminRoutes from './routes/AdminRoutes.js'
+// import ExpenseRoutes from './routes/expenseRoutes.js'
+// import IncomeRoutes from './routes/incomeRoutes.js'
+// dotenv.config()
+// const PORT = process.env.PORT || 3000
+// const app = express()
+
+// // mongo db 
+// DbCon()
+// app.use(express.json())
+// app.use(cookieparser())
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:5173'
+// }));
+
+// app.use('/api/auth', AuthRoutes)
+// app.use('/api/admin', AdminRoutes)
+// app.use('/api/expense', ExpenseRoutes)
+// app.use('/api/income', IncomeRoutes)
+
+// app.get('/', (req, res) => {
+//     res.send('test')
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`server is running on ${PORT}`)
+// })
+
+
+
+
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -5,8 +43,8 @@ import cookieParser from 'cookie-parser';
 import DbCon from './utlis/db.js'; // Corrected typo 'utlis' to 'utils'
 import AuthRoutes from './routes/Auth.js';
 import AdminRoutes from './routes/AdminRoutes.js';
-import ExpenseRoutes from './routes/ExpenseRoutes.js';
-import IncomeRoutes from './routes/IncomeRoutes.js';
+import ExpenseRoutes from './routes/expenseRoutes.js';
+import IncomeRoutes from './routes/incomeRoutes.js';
 
 dotenv.config();
 
@@ -15,7 +53,7 @@ const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN; // Used for development & produ
 
 const app = express();
 
-// MongoDB connectionn done
+// MongoDB connectionn doneyy
 DbCon();
 
 // Middleware setup
