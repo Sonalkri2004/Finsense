e79026459221f08a976ba9ecf511b4cf116c1b1a
Durgesh Prasad <sonalkumari000555@gmail.com>
2024-11-23 01:34:39 +0530

    Cheque no. added to Payvoucher

--- client/src/components/analytics/PayVoucher.jsx
+++ client/src/components/analytics/PayVoucher.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 import convertISOToDate from "../../utils/formatDate";
 
 const PayVoucher = React.forwardRef(({ transaction }, ref) => {
-  PayVoucher.displayName = 'PayVoucher';
+  PayVoucher.displayName = "PayVoucher";
   return (
     <div ref={ref} className="flex bg-white text-black flex-col h-[110vh]">
       <div className="w-full max-w-2xl mx-auto  p-4 h-full flex flex-col justify-between">
         <div className="text-center text-sm font-bold uppercase">
           Sunderwati Mahila Mahavidyalaya, Bhagalpur
           <div className="text-xs">(T.M. Bhagalpur University, Bhagalpur)</div>
-          <div className="font-bold text-sm border-b border-black pb-2">Pay Voucher</div>
+          <div className="font-bold text-sm border-b border-black pb-2">
+            Pay Voucher
+          </div>
         </div>
 
         <div className="flex-grow my-4">
           <div className="grid grid-cols-2 gap-2 mb-3 text-xs">
             <div className="flex">
               <span className="font-semibold">Voucher No.:</span>
-              <span className="ml-1">{transaction?.voucherNo || '____'}</span>
+              <span className="ml-1">{transaction?.voucherNo || "____"}</span>
             </div>
             <div className="flex">
               <span className="font-semibold">Name of Account:</span>
-              <span className="ml-1">{transaction?.bankName || '______________'}</span>
+              <span className="ml-1">
+                {transaction?.bankName || "______________"}
+              </span>
             </div>
             <div className="flex">
               <span className="font-semibold">Head:</span>
-              <span className="ml-1">{transaction?.subHead || '______________'}</span>
-              
+              <span className="ml-1">
+                {transaction?.subHead || "______________"}
+              </span>
             </div>
           </div>
 
@@ -39,22 +44,36 @@ const PayVoucher = React.forwardRef(({ transaction }, ref) => {
             </thead>
             <tbody>
               <tr className="border-b  border-black">
-                <td className="border-r align-top text-center border-black p-1 pt-5  h-8">{convertISOToDate(transaction?.updatedAt) || '____'}</td>
-                <td className="border-r align-top  text-center border-black p-1  pt-5 ">{transaction?.purpose || '______________'}</td>
-                <td className="p-1  align-top  text-center  pt-5 ">₹ {parseInt(transaction?.amount).toFixed(2) || '____'}</td>
+                <td className="border-r align-top text-center border-black p-1 pt-8  h-8">
+                  {convertISOToDate(transaction?.updatedAt) || "____"}
+                </td>
+                <td className="border-r align-top  text-center border-black p-1  pt-10 ">
+                  {" "}
+                  <p className="mt-[-34px] mb-2 underline">To be Paid</p>
+                  {transaction?.purpose || "______________"}{" "}
+                  <div className="flex justify-center mt-52">
+                    <span className="font-semibold">Cheque No.:</span>
+                    <span className="ml-1">
+                      {transaction?.TxnId || "______________"}
+                    </span>
+                  </div>
+                </td>
+                <td className="p-1  align-top  text-center  pt-8 ">
+                  ₹ {parseInt(transaction?.amount).toFixed(2) || "____"}
+                </td>
               </tr>
-              
             </tbody>
           </table>
 
-          <div className="flex justify-between items-center mb-3 text-xs">
+          <div className="flex justify-end items-center mb-3 text-xs">
             <span className="font-semibold">Grand Total</span>
-          
           </div>
 
-          <div className="flex justify-between mb-4 text-xs">
+          <div className="flex justify-end mb-4 text-xs">
             <div>
-            <span>₹ {parseInt(transaction?.total).toFixed(2) || '_____________'}</span>
+              <span>
+                ₹ {parseInt(transaction?.total).toFixed(2) || "_____________"}
+              </span>
             </div>
           </div>
         </div>
@@ -64,15 +83,15 @@ const PayVoucher = React.forwardRef(({ transaction }, ref) => {
             Accountant
             {/* <div className="mt-4">Skillancer Pvt. Ltd.</div> */}
           </div>
-          <div className="text-center">
-            Bursar
-          </div>
+          <div className="text-center">Bursar</div>
           <div className="text-center">
             Principal
             <div className="mt-4">S.M. College, Bhagalpur</div>
           </div>
         </div>
+        <p className="text-center text-xs mt-4">This is a system generated voucher.</p>
       </div>
+     
     </div>
   );
 });
--- client/src/pages/TransactionsPage.jsx
+++ client/src/pages/TransactionsPage.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/common/Header";
 import OverviewCards from "../components/analytics/OverviewCards";
 import TransactionTable from "../components/transactions/TransactionsTable";
-
+import NoteSheet from "../components/analytics/NoteSheet";
 const AnalyticsPage = () => {
 	return (
 		<div className='flex-1 overflow-auto relative z-10 bg-gray-900'>
@@ -10,7 +10,8 @@ const AnalyticsPage = () => {
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
 				<OverviewCards />
 				<TransactionTable />
-				
+
+				<NoteSheet/>
 			</main>
 		</div>
 	);
