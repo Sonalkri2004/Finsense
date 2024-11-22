b6cb2ef5e0005b2876b07f2540d59a8ba22034fa
Durgesh Prasad <sonalkumari000555@gmail.com>
2024-11-22 22:57:33 +0530

    Pending table responsiveness fixed

--- client/src/components/pending/PendingTable.jsx
+++ client/src/components/pending/PendingTable.jsx
@@ -15,16 +15,16 @@ const PendingTable = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [confirmationIsOpen, setConfirmationIsOpen] = useState(false);
   const [confirmationAction, setConfirmationAction] = useState(null);
-  const [transactionId, setTransactionId] = useState('');
-  
+  const [transactionId, setTransactionId] = useState("");
+
   const [commentForm, setCommentForm] = useState({
-    expenseId: '',
-    commentText: '',
-  })
+    expenseId: "",
+    commentText: "",
+  });
 
   useEffect(() => {
-    setCommentForm({ ...commentForm, expenseId: selectedTransaction?._id })
-  }, [selectedTransaction])
+    setCommentForm({ ...commentForm, expenseId: selectedTransaction?._id });
+  }, [selectedTransaction]);
 
   const openModal = (transaction) => {
     setSelectedTransaction(transaction);
@@ -34,9 +34,9 @@ const PendingTable = () => {
   const closeModal = () => {
     setModalIsOpen(false);
     setCommentForm({
-      expenseId: '', 
-      commentText: ''
-    }); 
+      expenseId: "",
+      commentText: "",
+    });
     setSelectedTransaction(null);
   };
 
@@ -65,8 +65,8 @@ const PendingTable = () => {
             withCredentials: true,
           }
         );
-        console.log("get Expense = " , response )
-        
+        console.log("get Expense = ", response);
+
         // if (response.data) {
         //   const filteredTransactions = response.data.Expenses.filter(transaction => transaction.status == "approved")
         //   console.log("Filtered", filteredTransactions)
@@ -78,8 +78,6 @@ const PendingTable = () => {
     };
 
     fetchExpenses();
-
-    
   }, [modalIsOpen]);
   // Calculate current transactions for the current page
   const indexOfLastTransaction = currentPage * itemsPerPage;
@@ -107,37 +105,37 @@ const PendingTable = () => {
 
   return (
     <motion.div
-      className="relative bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border border-gray-700"
+      className="relative bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-4 md:p-6 border border-gray-700"
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.4 }}
     >
-      <div className="flex flex-col justify-between items-start mb-6 gap-2">
-        <h2 className="text-xl font-semibold text-gray-100">
+      <div className="flex flex-col justify-between items-start mb-4 md:mb-6 gap-2">
+        <h2 className="text-lg md:text-xl font-semibold text-gray-100">
           Pending Transactions
         </h2>
       </div>
 
       <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-700">
+        <table className="min-w-full divide-y divide-gray-700 text-xs md:text-sm">
           <thead>
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-left font-medium text-gray-400 uppercase tracking-wider">
                 Txn ID
               </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-left font-medium text-gray-400 uppercase tracking-wider">
                 Date
               </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-left font-medium text-gray-400 uppercase tracking-wider">
                 SubHead
               </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-left font-medium text-gray-400 uppercase tracking-wider">
                 Total
               </th>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-left font-medium text-gray-400 uppercase tracking-wider">
                 Status
               </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-400 uppercase tracking-wider">
+              <th className="px-4 md:px-6 py-2 md:py-3 text-center font-medium text-gray-400 uppercase tracking-wider">
                 View
               </th>
             </tr>
@@ -151,39 +149,43 @@ const PendingTable = () => {
                 animate={{ opacity: 1 }}
                 transition={{ duration: 0.3 }}
               >
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-100">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-gray-100 font-medium">
                   {transaction?._id.slice(0, 10)}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-gray-300">
                   {convertISOToDate(transaction?.updatedAt)}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-100">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-gray-100 font-medium">
                   {transaction?.subHead}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-100">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-gray-100 font-medium">
                   ₹ {parseInt(transaction?.total).toFixed(2)}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-gray-300">
                   <span
-                    className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${transaction.status === "verified"
-                      ? "bg-green-100 text-green-800"
-                      : transaction.status === "pending"
+                    className={`px-2 inline-flex text-xs md:text-sm leading-5 font-semibold rounded-full ${
+                      transaction.status === "verified"
+                        ? "bg-green-100 text-green-800"
+                        : transaction.status === "pending"
                         ? "bg-yellow-100 text-yellow-800"
                         : transaction.status === "approved"
-                          ? "bg-blue-100 text-blue-800"
-                          : transaction.status === "completed"
-                            ? "bg-purple-100 text-purple-800"
-                            : transaction.status === "rejected"
-                              ? "bg-red-100 text-red-800"
-                              : "bg-gray-100 text-gray-800" // Default case if status does not match
-                      }`}
+                        ? "bg-blue-100 text-blue-800"
+                        : transaction.status === "completed"
+                        ? "bg-purple-100 text-purple-800"
+                        : transaction.status === "rejected"
+                        ? "bg-red-100 text-red-800"
+                        : "bg-gray-100 text-gray-800"
+                    }`}
                   >
                     {transaction?.status}
                   </span>
                 </td>
 
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-center text-gray-300">
-                  <button onClick={() => openModal(transaction)} className="text-indigo-400 hover:text-indigo-300 mr-2">
+                <td className="px-4 md:px-6 py-2 md:py-4 text-center text-gray-300">
+                  <button
+                    onClick={() => openModal(transaction)}
+                    className="text-indigo-400 hover:text-indigo-300 mr-2"
+                  >
                     <Eye size={18} />
                   </button>
                 </td>
@@ -194,19 +196,23 @@ const PendingTable = () => {
       </div>
 
       {/* Pagination Controls */}
-      <div className="flex justify-between items-center mt-4">
+      <div className="flex flex-col md:flex-row justify-between items-center mt-4 gap-2 md:gap-0">
         <button
-          className={`px-4 py-2 text-white bg-blue-600 rounded ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""}`}
+          className={`px-3 md:px-4 py-1.5 md:py-2 text-xs md:text-sm text-white bg-blue-600 rounded ${
+            currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
+          }`}
           onClick={handlePreviousPage}
           disabled={currentPage === 1}
         >
           Previous
         </button>
-        <div className="text-gray-400">
+        <div className="text-gray-400 text-xs md:text-sm">
           Page {currentPage} of {totalPages}
         </div>
         <button
-          className={`px-4 py-2 text-white bg-blue-600 rounded ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""}`}
+          className={`px-3 md:px-4 py-1.5 md:py-2 text-xs md:text-sm text-white bg-blue-600 rounded ${
+            currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""
+          }`}
           onClick={handleNextPage}
           disabled={currentPage === totalPages}
         >
